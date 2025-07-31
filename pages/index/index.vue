23f07e075ef9c10f5cc9b81af2c332ac36fbace1
王鼎淇 <2070651521@qq.com>
2025-07-31 16:30:08 +0800

    Initial commit

---
+++ pages/index/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <view class="pet-container">
+    模板结构
+
+  </view>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+  @import "../../petcareui/pet-global.css";
+</style>
